c5f97491cb47f531f69a4ab8d4f778b178d1da64
scorpion43 <scorpion7812@gmail.com>
2017-08-31 19:16:10 +0200

    Wstępna działająca implementacja obsługi konta moderatora

---
+++ src/main/java/pl/dn/dao/placeInfo/CityDao.java
@@ -0,0 +1,7 @@
+package pl.dn.dao.placeInfo;
+
+/**
+ * Created by User on 30.08.2017.
+ */
+public class CityDao {
+}
---
+++ src/main/java/pl/dn/service/userType/ModeratorService.java
@@ -0,0 +1,7 @@
+package pl.dn.service.userType;
+
+/**
+ * Created by User on 31.08.2017.
+ */
+public class ModeratorService {
+}
