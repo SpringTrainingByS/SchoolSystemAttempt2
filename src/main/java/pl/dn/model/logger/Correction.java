6524e84da2b5e7e76a3e90ba7444ec6c5d17de3c
Dariusz Nurzyński <scorpion7812@gmail.com>
2017-11-18 12:34:00 +0100

    Korekty

---
+++ src/main/java/pl/dn/model/logger/Correction.java
@@ -0,0 +1,74 @@
+package pl.dn.model.logger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import pl.dn.model.userType.User;
+
+@Entity
+public class Correction {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "class_name_id")
+	private CorrectionClassName className;
+	
+	@ManyToOne
+	@JoinColumn(name = "class_field_id")
+	private CorrectionFieldName fieldName;
+	
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public CorrectionClassName getClassName() {
+		return className;
+	}
+
+	public void setClassName(CorrectionClassName className) {
+		this.className = className;
+	}
+
+	public CorrectionFieldName getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(CorrectionFieldName fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
